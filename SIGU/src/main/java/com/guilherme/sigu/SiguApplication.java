676ed6b48f2517f71b82f4b0072dd140a33a5bd9
Guilherme <guilhermebha1989@gmail.com>
2022-03-08 21:45:32 -0300

    Compartilhando projeto

---
+++ SIGU/src/main/java/com/guilherme/sigu/SiguApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.sigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiguApplication.class, args);
+	}
+
+}
---
+++ SIGU/src/test/java/com/guilherme/sigu/SiguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.sigu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
